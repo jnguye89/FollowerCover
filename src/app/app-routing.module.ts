137d2eb73bb3b67fb6627f755148080ec562069c
Jackie Nguyen <jnguye89@gmail.com>
2023-06-05 21:54:37 -0500

    More changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: MainComponent
+  }, {
+    path: 'home',
     component: HomeComponent
   }
 ];
